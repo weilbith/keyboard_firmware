f061d4d1c40ad211f54e5e1c3d506737fb4fa4b3
Thore Weilbier <thore@weilbier.net>
2019-11-22 11:26:13 +0100

    Explicitly enable action configurations

--- thore/config.h
+++ thore/config.h
@@ -1,2 +1,7 @@
 #define PERMISSIVE_HOLD
-#define RETRO_TAPPING
+
+// Make sure that some options are not disabled by the keyboard configuration.
+#undef IGNORE_MOD_TAP_INTERRUPT
+#undef NO_ACTION_LAYER
+#undef NO_ACTION_TAPPING
+#undef NO_ACTION_ONESHOT
