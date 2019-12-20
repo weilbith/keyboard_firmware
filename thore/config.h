a2d0172f05c811037f9d934440a82dd2ec9c70e4
Thore Weilbier <thore@weilbier.net>
2019-12-20 16:42:09 +0100

    Feature: Reduce default DEBOUNCE for performance

--- thore/config.h
+++ thore/config.h
@@ -1,5 +1,8 @@
 #define PERMISSIVE_HOLD
 
+#undef DEBOUNCE
+#define DEBOUNCE 2
+
 // Make sure that some options are not disabled by the keyboard configuration.
 #undef IGNORE_MOD_TAP_INTERRUPT
 #undef NO_ACTION_LAYER
