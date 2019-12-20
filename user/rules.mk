d39c055a0c8fdbacf1453a13d00d5db93f901d95
Thore Weilbier <thore@weilbier.net>
2019-12-20 18:50:45 +0100

    Feature: Allow simple layer and modifier colors
    Export the functionality to set the backlight color by the active layer and
    modifier to the user space. This allows to configure the behavior with
    two simple mappings within the key mapping.
    
    Active modifiers take preference over active layers. If no modifier is
    active or it has no color defined, the color of the layer gets used. If
    the active layer has no color defined, no backlight is enabled.

--- user/rules.mk
+++ user/rules.mk
@@ -1,3 +1,3 @@
-SRC += tap_dance_simulations.c
+SRC += tap_dance_simulations.c backlight.c
 
 TAP_DANCE_ENABLE = yes
--- user/thore.h
+++ user/thore.h
@@ -3,3 +3,7 @@
 #ifdef TAP_DANCE_ENABLE
   #include "tap_dance_simulations.h"
 #endif
+
+#ifdef RGBLIGHT_ENABLE
+  #include "backlight.h"
+#endif
