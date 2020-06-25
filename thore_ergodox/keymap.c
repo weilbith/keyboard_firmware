d33c860c44a7dfbcfc01475e251ade17ec76add1
Thore Weilbier <thore@weilbier.net>
2020-06-25 09:37:24 +0200

    Add layer for Workman layout

--- thore_ergodox/keymap.c
+++ thore_ergodox/keymap.c
@@ -2,9 +2,10 @@
 #include "thore.h"
 
 enum layer_names {
-  LAYER_BASE  = 0,
+  LAYER_QWERTY = 0,
   LAYER_SYMBOLS,
   LAYER_NUMBERS,
+  LAYER_WORKMAN,
 };
 
 enum custom_keycodes {
@@ -29,20 +30,24 @@ enum tab_dance_codes {
 };
 
 qk_tap_dance_action_t tap_dance_actions[] = {
-  [F_CTRL] = ACTION_TAP_DANCE_MT(KC_F, KC_LCTL),
-  [J_CTRL] = ACTION_TAP_DANCE_MT(KC_J, KC_LCTL),
-  [D_ALT]  = ACTION_TAP_DANCE_MT(KC_D, KC_LALT),
-  [K_ALT]  = ACTION_TAP_DANCE_MT(KC_K, KC_LALT),
+  [F_CTRL] = ACTION_TAP_DANCE_MT(KC_F, KC_LCTL), // QWERTY
+  [T_CTRL] = ACTION_TAP_DANCE_MT(KC_T, KC_LCTL), // Workman
+  [J_CTRL] = ACTION_TAP_DANCE_MT(KC_J, KC_LCTL), // QWERTY
+  [N_CTRL] = ACTION_TAP_DANCE_MT(KC_N, KC_LCTL), // Workman
+  [D_ALT]  = ACTION_TAP_DANCE_MT(KC_D, KC_LALT), // QWERTY
+  [H_ALT]  = ACTION_TAP_DANCE_MT(KC_H, KC_LALT), // Workman
+  [K_ALT]  = ACTION_TAP_DANCE_MT(KC_K, KC_LALT), // QWERTY
+  [E_ALT]  = ACTION_TAP_DANCE_MT(KC_E, KC_LALT), // Workman
 };
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [LAYER_BASE] = LAYOUT_ergodox_pretty(
-    KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,                                          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_PSCREEN,
+  [LAYER_QWERTY] = LAYOUT_ergodox_pretty(
+    TG(LAYER_WORKMAN), KC_NO,       KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,                                          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_PSCREEN,
     KC_NO,          KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           KC_NO,                                          KC_NO,          KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_NO,
     ESCAPE_SHIFT,   KC_A,           KC_S,           TD(D_ALT),      TD(F_CTRL),     KC_G,                                                                           KC_H,           TD(J_CTRL),     TD(K_ALT),      KC_L,           KC_SCOLON,      DELETE_SHIFT,
     KC_NO,          KC_Z,           KC_X,           KC_C,           KC_V,           KC_NO,          KC_NO,                                          KC_NO,          KC_NO,          KC_M,           KC_COMMA,       KC_DOT,         KC_SLASH,       KC_NO,
-    KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_B,                                                                           KC_N,           KC_NO,          KC_NO,          KC_NO,          KC_NO,
+    KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_B,                                                                                                           KC_N,           KC_NO,          KC_NO,          KC_NO,          KC_NO,
                                                                                                     KC_NO,          KC_NO,          KC_NO,          KC_NO,
                                                                                                                     KC_NO,          KC_NO,
                                                                                     BSPACE_SYMBOLS, TAB_NUMBERS,    OSM(MOD_LGUI),  LALT(KC_GRAVE), ENTER_NUMBERS,  SPACE_SYMBOLS
@@ -61,14 +66,25 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [LAYER_NUMBERS] = LAYOUT_ergodox_pretty(
-    KC_TRANSPARENT, KC_F1,          KC_F2,          KC_F3,          KC_F4,          KC_F5,          KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_F6,          KC_F7,          KC_F8,          KC_F9,          KC_F10,         KC_TRANSPARENT,
+    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
     KC_TRANSPARENT, KC_1,           KC_2,           KC_3,           KC_4,           KC_5,           KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           KC_TRANSPARENT,
-    KC_TRANSPARENT, KC_TRANSPARENT, KC_HOME,        KC_PGDOWN,       KC_PGUP,        KC_END,                                                                         KC_LEFT,        KC_DOWN,        KC_UP,          KC_RIGHT,       KC_TRANSPARENT, KC_TRANSPARENT,
+    KC_TRANSPARENT, KC_TRANSPARENT, KC_HOME,        KC_PGDOWN,      KC_PGUP,        KC_END,                                                                         KC_LEFT,        KC_DOWN,        KC_UP,          KC_RIGHT,       KC_TRANSPARENT, KC_TRANSPARENT,
     KC_TRANSPARENT, KC_F1,          KC_F2,          KC_F3,          KC_F4,          KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_F7,          KC_F8,          KC_F9,          KC_F10,         KC_TRANSPARENT,
     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_F5,                                                                                                          KC_F6,          KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
                                                                                                     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
                                                                                                                     KC_TRANSPARENT, KC_TRANSPARENT,
                                                                                     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT
+  ),
+
+  [LAYER_WORKMAN] = LAYOUT_ergodox_pretty(
+    KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,                                          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_PSCREEN,
+    KC_NO,          KC_Q,           KC_D,           KC_R,           KC_W,           KC_B,           KC_NO,                                          KC_NO,          KC_J,           KC_F,           KC_U,           KC_P,           KC_SCOLON,      KC_NO,
+    ESCAPE_SHIFT,   KC_A,           KC_S,           TD(H_ALT),      TD(T_CTRL),     KC_G,                                                                           KC_Y,           TD(N_CTRL),     TD(E_ALT),      KC_O,           KC_I,           DELETE_SHIFT,
+    KC_NO,          KC_Z,           KC_X,           KC_M,           KC_C,           KC_NO,          KC_NO,                                          KC_NO,          KC_NO,          KC_L,           KC_COMMA,       KC_DOT,         KC_SLASH,       KC_NO,
+    KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_V,                                                                                                           KC_K,           KC_NO,          KC_NO,          KC_NO,          KC_NO,
+                                                                                                    KC_NO,          KC_NO,          KC_NO,          KC_NO,
+                                                                                                                    KC_NO,          KC_NO,
+                                                                                    BSPACE_SYMBOLS, TAB_NUMBERS,    OSM(MOD_LGUI),  LALT(KC_GRAVE), ENTER_NUMBERS,  SPACE_SYMBOLS
   ),
 };
 
@@ -82,6 +98,7 @@ backlight_hsv_color_t backlight_modifier_colors[] = {
 backlight_hsv_color_t backlight_layer_colors[] = {
   [LAYER_SYMBOLS] = { HSV_BLUE },
   [LAYER_NUMBERS] = { HSV_PINK },
+  [LAYER_WORKMAN] = { HSV_ORANGE },
 };
 
 
@@ -93,11 +110,3 @@ void matrix_scan_user(void) {
 
   backlight_apply_for_modifier_and_layer();
 };
-
-void keyboard_post_init_user(void) {
-  // Customise these values to desired behaviour
-  /* debug_enable=true; */
-  /* debug_matrix=true; */
-  //debug_keyboard=true;
-  //debug_mouse=true;
-}
