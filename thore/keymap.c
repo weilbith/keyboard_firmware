0fb14ccda79a1104d4e5f14cc1c78e6133b10fa8
Thore Strassburg <thore@weilbier.net>
2021-03-19 11:05:34 +0100

    Replace TMux prefix key with delete tap
    The delete key got lost recently when reducing the total number of used
    keys from 38 to 36. But it has turned out that it is quite annoying to
    not have it.
    The only key that was still kinda reserved and special was the TMux
    prefix key. Not even the OS (GUI key) control key has it's own dedicated
    key anymore. Therefore the TMux configuration got adopted to use prefix
    that is easy to type with regular modifiers (i.e. `Ctrl-a` at the time
    of writing). Therefore the key can now be used for the delete tap.
    It makes sense to have the key on the right thumb cluster as it an
    action that targets the right side of the cursor.

--- thore/keymap.c
+++ thore/keymap.c
@@ -38,7 +38,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_B,                                                                                                           KC_N,           KC_NO,          KC_NO,          KC_NO,          KC_NO,
                                                                                                     KC_NO,          KC_NO,          KC_NO,          KC_NO,
                                                                                                                     KC_NO,          KC_NO,
-                                                                                    BSPACE_SYMBOLS, TAB_NUMBERS,    KC_ESCAPE,      LALT(KC_GRAVE), ENTER_NUMBERS,  SPACE_SYMBOLS
+                                                                                    BSPACE_SYMBOLS, TAB_NUMBERS,    KC_ESCAPE,      KC_DELETE,      ENTER_NUMBERS,  SPACE_SYMBOLS
   ),
   
 
@@ -72,6 +72,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_V,                                                                                                           KC_K,           KC_NO,          KC_NO,          KC_NO,          KC_NO,
                                                                                                     KC_NO,          KC_NO,          KC_NO,          KC_NO,
                                                                                                                     KC_NO,          KC_NO,
-                                                                                    BSPACE_SYMBOLS, TAB_NUMBERS,    KC_ESCAPE,      LALT(KC_GRAVE), ENTER_NUMBERS,  SPACE_SYMBOLS
+                                                                                    BSPACE_SYMBOLS, TAB_NUMBERS,    KC_ESCAPE,      KC_DELETE,      ENTER_NUMBERS,  SPACE_SYMBOLS
   ),
 };
