6a6a5f3c618b8e4f741020a34758359cc6b9f566
Thore Strassburg <thore@weilbier.net>
2022-01-03 17:42:12 +0100

    add custom layout for 34 key ergodox
    As I only use 34 keys of my ergodox, it is a bit annoying that all the
    keymaps are so huge with 50% of the positions just being `KC_NO` or
    `KC_TRANSPARENT`. Having this custom layout allows to shrink this a lot.

---
+++ layouts/ergodox/thore/layout.h
@@ -0,0 +1,28 @@
+#define LAYOUT_ergodox_34_keys( \
+  k00, k01, k02, k03, k04, \
+  k10, k11, k12, k13, k14, \
+  k20, k21, k22, k23,      \
+            k28, k29, k30, \
+                           \
+  k05, k06, k07, k08, k09, \
+  k15, k16, k17, k18, k19, \
+       k24, k25, k26, k27, \
+  k31, k32, k33            \
+) { \
+    { KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO }, \
+    { KC_NO, k00,   k10,   k20,   KC_NO, KC_NO }, \
+    { KC_NO, k01,   k11,   k21,   KC_NO, k30   }, \
+    { KC_NO, k02,   k12,   k22,   KC_NO, k29   }, \
+    { KC_NO, k03,   k13,   k23,   k28,   KC_NO }, \
+    { KC_NO, k04,   k14,   KC_NO, KC_NO, KC_NO }, \
+    { KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO }, \
+                                                  \
+    { KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO }, \
+    { KC_NO, k05,   k15,   KC_NO, KC_NO, KC_NO }, \
+    { KC_NO, k06,   k16,   k24,   k33,   KC_NO }, \
+    { KC_NO, k07,   k17,   k25,   KC_NO, k32   }, \
+    { KC_NO, k08,   k18,   k26,   KC_NO, k31   }, \
+    { KC_NO, k09,   k19,   k27,   KC_NO, KC_NO }, \
+    { KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO }  \
+  }
+
